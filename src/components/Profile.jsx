0a11f6105e74b6a66a28cc5e4209723e16138b81
Sanskar-12 <sanskarv2004@gmail.com>
2025-10-12 16:01:02 +0530

    Auth with Google added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,6 +9,8 @@ import { TiUserAddOutline } from "react-icons/ti";
 import { MdOutlineSwitchAccount } from "react-icons/md";
 import { SiYoutubestudio } from "react-icons/si";
 import { FiLogOut } from "react-icons/fi";
+import { auth, provider } from "../utils/firebase.js";
+import { signInWithPopup } from "firebase/auth";
 
 const Profile = () => {
   const { user } = useSelector((state) => state.user);
@@ -27,6 +29,34 @@ const Profile = () => {
     } catch (error) {
       console.log(error);
       toast.error("Error in SignOut");
+    }
+  };
+
+  const handleAuthWithGoogle = async () => {
+    try {
+      const response = await signInWithPopup(auth, provider);
+      console.log(response);
+
+      const formData = new FormData();
+
+      formData.append("userName", response.user.displayName);
+      formData.append("email", response.user.email);
+      formData.append("photoUrl", response.user.photoURL);
+
+      const { data } = await axios.post(
+        `${serverUrl}/api/v1/auth/google`,
+        formData,
+        {
+          withCredentials: true,
+        }
+      );
+
+      dispatch(setUser(data.user));
+
+      toast.success(data.message);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error in Google Auth");
     }
   };
   return (
@@ -52,7 +82,7 @@ const Profile = () => {
         <div className="flex flex-col py-2">
           <button
             className="flex items-center gap-3 px-4 py-2 hover:bg-gray-700"
-            // onClick={googleSignIn}
+            onClick={handleAuthWithGoogle}
           >
             <FcGoogle className="text-xl" />
             SignIn with Google Account
