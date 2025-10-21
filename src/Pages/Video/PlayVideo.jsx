60d2ea3806136455058d54f950ad07809db53c63
Sanskar-12 <sanskarv2004@gmail.com>
2025-10-21 21:27:16 +0530

    Adding Video Player

--- src/Pages/Video/PlayVideo.jsx
+++ src/Pages/Video/PlayVideo.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { FaBackward, FaForward, FaPause, FaPlay } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -7,8 +8,29 @@ const PlayVideo = () => {
   const { id } = useParams();
   const [video, setVideo] = useState(null);
   const [channel, setChannel] = useState(null);
+  const [showControls, setShowControls] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
 
   const { allVideosData } = useSelector((state) => state.content);
+
+  const onTogglePlay = () => {
+    if (!videoRef.current) return;
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  };
+
+  const skipForward = () => {
+    if (!videoRef.current) return;
+    videoRef.current.currentTime += 10;
+  };
+
+  const skipBackward = () => {
+    if (!videoRef.current) return;
+    videoRef.current.currentTime -= 10;
+  };
 
   useEffect(() => {
     if (!allVideosData) {
@@ -27,15 +49,45 @@ const PlayVideo = () => {
     <div className="flex bg-[#0f0f0f] text-white flex-col lg:flex-row gap-6 p-4 lg:p-6">
       <div className="flex-1">
         {/* Video Player */}
-        <div className="w-full aspect-video bg-black rounded-lg overflow-hidden relative">
+        <div
+          onMouseEnter={() => setShowControls(true)}
+          onMouseLeave={() => setShowControls(false)}
+          className="w-full aspect-video bg-black rounded-lg overflow-hidden relative"
+        >
           <video
             src={video?.videoUrl}
             className="w-full h-full object-contain"
             controls={false}
             autoPlay
             ref={videoRef}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}
           />
+          {showControls && (
+            <div className="absolute inset-0 hidden lg:flex items-center justify-center gap-6 sm:gap-10 transition-opacity duration-300 z-20">
+              <button
+                className="bg-black/70 p-3 sm:p-4 rounded-full hover:bg-orange-600 transition"
+                onClick={skipBackward}
+              >
+                <FaBackward size={24} />
+              </button>
+              <button
+                className="bg-black/70 p-3 sm:p-4 rounded-full hover:bg-orange-600 transition"
+                onClick={() => onTogglePlay()}
+              >
+                {isPlaying ? <FaPause size={28} /> : <FaPlay size={28} />}
+              </button>
+              <button
+                className="bg-black/70 p-3 sm:p-4 rounded-full hover:bg-orange-600 transition"
+                onClick={skipForward}
+              >
+                <FaForward size={24} />
+              </button>
+            </div>
+          )}
         </div>
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 via-black/60 to-transparent px-2 sm:px-4 py-2 z-30">
+        <input type="range" min={0} max={}/></div>
       </div>
     </div>
   );
